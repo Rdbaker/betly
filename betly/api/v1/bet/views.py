6c01a0122b054d0de2a72b8aaba1ff542bf1e561
wassaf <Wassaf@gmail.com>
2016-08-27 18:19:00 -0400

    create a bet

--- betly/api/v1/bet/views.py
+++ betly/api/v1/bet/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from flask_login import login_required, current_user
 from webargs import fields
 from webargs.flaskparser import use_args
 from werkzeug.exceptions import BadRequest
@@ -14,3 +15,12 @@ BET_SCHEMA = BetSchema()
 def list_bets(args=None):
     bets = Bet.query.all()
     return BET_SCHEMA.dumps(bets).data
+
+
+@blueprint.route('/create', methods=['POST'], strict_slashes=False)
+@login_required
+def create_bet():
+    bet_data = BET_SCHEMA().load(request.json())
+    bet_data.data['organizer'] = current_user
+    Bet.create(**bet_data.data)
+    return bet_data.data
